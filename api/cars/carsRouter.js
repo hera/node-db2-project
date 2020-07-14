0362220d5edf01135a380b76557f93c460d66ae4
hera <a.heraimenka@gmail.com>
2020-07-14 13:56:15 -0700

    Get a car by id

--- api/cars/carsRouter.js
+++ api/cars/carsRouter.js
@@ -3,6 +3,9 @@ const carsDb = require("../../data/carsDb");
 
 const router = express.Router();
 
+
+// Get all cars
+
 router.get("/", (req, res) => {
     carsDb.get()
         .then(cars => {
@@ -11,6 +14,28 @@ router.get("/", (req, res) => {
         .catch(error => {
             res.status(500).json({
                 error: "Server error. Could not get all cars.",
+                description: error
+            });
+        });
+});
+
+
+// Get a car by id
+
+router.get("/:id", (req, res) => {
+    carsDb.getById(req.params.id)
+        .then(cars => {
+            if (cars.length) {
+                res.status(200).json(cars);
+            } else {
+                res.status(404).json({
+                    error: `Not found. Car with id ${req.params.id} does not exist.`
+                });
+            }
+        })
+        .catch(error => {
+            res.status(500).json({
+                error: `Server error. Could not get a car with id ${req.params.id}`,
                 description: error
             });
         });
--- data/carsDb.js
+++ data/carsDb.js
@@ -3,9 +3,14 @@ const db = require("./dbConfig");
 const TABLE_NAME = "cars";
 
 module.exports = {
-    get
+    get,
+    getById
 }
 
 function get () {
     return db(TABLE_NAME);
+}
+
+function getById (id) {
+    return db(TABLE_NAME).where({id});
 }
