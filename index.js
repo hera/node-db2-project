9eb36074a84b180f94ffbc3701b40ac0b0e40628
hera <a.heraimenka@gmail.com>
2020-07-14 14:51:27 -0700

    Switch to postgres

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const carsRouter = require("./api/cars/carsRouter");
 
--- knexfile.js
+++ knexfile.js
@@ -1,51 +1,23 @@
-// Update with your config settings.
+require("dotenv").config();
 
 module.exports = {
-
-  development: {
-    client: "sqlite3",
-    connection: {
-      filename: "./data/dealership.db3"
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: "./data/migrations"
-    },
-    seeds: {
-      directory: "./data/seeds"
+    development: {
+        client: "pg",
+        connection: {
+            database: "test",
+            user: "test_user",
+            password: process.env.DB_PASSWORD
+        },
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            tableName: "knex_migrations",
+            directory: "./data/migrations"
+        },
+        seeds: {
+            directory: "./data/seeds"
+        }
     }
-  },
-/*
-  staging: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user:     "username",
-      password: "password"
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: "knex_migrations"
-    }
-  },
-
-  production: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user:     "username",
-      password: "password"
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: "knex_migrations"
-    }
-  }
-*/
 };
